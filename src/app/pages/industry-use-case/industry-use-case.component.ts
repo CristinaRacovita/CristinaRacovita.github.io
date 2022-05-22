47a427c4cc6731dc79ef22c567525ed0c38e7313
Cristina Racovita <cristina.racovita98@gmail.com>
2022-05-22 09:58:17 +0300

    fix firebase problem

--- src/app/pages/industry-use-case/industry-use-case.component.ts
+++ src/app/pages/industry-use-case/industry-use-case.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { IndustryCsv } from 'src/app/shared/models/industry-csv.enum';
+import { Industry } from 'src/app/shared/models/industry.enum';
+import { IUseCase } from 'src/app/shared/models/useCase.model';
 import { CsvService } from 'src/app/shared/services/csv.service';
+import { IndustryService } from 'src/app/shared/services/industry.service';
 import { SectionService } from 'src/app/shared/services/section.service';
 
 @Component({
@@ -8,10 +12,47 @@ import { SectionService } from 'src/app/shared/services/section.service';
   templateUrl: './industry-use-case.component.html',
   styleUrls: ['./industry-use-case.component.scss'],
 })
-export class IndustryUseCaseComponent {
+export class IndustryUseCaseComponent implements OnInit, OnDestroy {
   public data: Observable<any> = this.csvService.processFile(
     this.sectionService.activeUseCase.value
   );
+  public useCases: IUseCase[] = [];
+  public industryName: string = '';
+  private subscription = new Subscription();
 
-  public constructor(private csvService: CsvService, private sectionService: SectionService) {}
+  public constructor(
+    private csvService: CsvService,
+    private sectionService: SectionService,
+    public industryService: IndustryService
+  ) {}
+
+  public ngOnInit(): void {
+    const indexOfUseCase: number = Object.values(IndustryCsv).indexOf(
+      this.sectionService.activeUseCase.value as IndustryCsv
+    );
+
+    const keyUseCase = Object.keys(IndustryCsv)[indexOfUseCase];
+    const industryName: string | undefined = Object.keys(Industry).find(
+      (x) => x == keyUseCase
+    );
+
+    if (industryName) {
+      this.industryName = industryName.toLowerCase();
+      this.subscribeToUseCases(this.industryName);
+    }
+  }
+
+  private subscribeToUseCases(industryName: string): void {
+    this.subscription = this.industryService
+      .getUseCasesByIndustryName(industryName.toLowerCase())
+      .subscribe((useCases: IUseCase[] | undefined) => {
+        if (useCases) {
+          this.useCases = useCases;
+        }
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/shared/services/industry.service.ts
+++ src/app/shared/services/industry.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { IIndustry } from '../models/industry.model';
+import { IUseCase } from '../models/useCase.model';
+import { collection, query, where } from 'firebase/firestore';
+import { IndustryCsv } from '../models/industry-csv.enum';
+import { Industry } from '../models/industry.enum';
 
 @Injectable()
 export class IndustryService {
@@ -9,5 +14,20 @@ export class IndustryService {
 
   public getIndustries(): Observable<IIndustry[]> {
     return this.db.list('industries').valueChanges() as Observable<IIndustry[]>;
+  }
+
+  public getUseCasesByIndustryName(
+    industryName: string
+  ): Observable<IUseCase[] | undefined> {
+    let industries = this.db.list('industries').valueChanges() as Observable<
+      IIndustry[]
+    >;
+    return industries.pipe(
+      map(
+        (industriesResult: IIndustry[]) =>
+          industriesResult?.find((industry) => industry.name === industryName)
+            ?.useCases
+      )
+    );
   }
 }
